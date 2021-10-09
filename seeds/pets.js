bd41db3259d404262ae37999e5f66f25a3f712a6
Edison Li <47466335+edison4354@users.noreply.github.com>
2021-10-08 21:58:30 -0700

    Updated Images

--- seeds/pets.js
+++ seeds/pets.js
@@ -1,12 +1,12 @@
 module.exports = [
     {
         "name": "Rex",
-        "species": "Rottweiler",
-        "birthday": "20017-11-11",
+        "species": "German Shepherd",
+        "birthday": "2007-11-11",
         "favoriteFood": "Chicken",
-        "picUrl": "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Rottweiler-3.jpg",
+        "picUrl": "https://www.dogbreedslist.info/uploads/dog-pictures/german-shepherd-dog-2.jpg",
         "price": 9.99,
-        "picUrlSq": "http://www.dogbreedplus.com/dog_breeds/images/cute-rottweiler-puppy.jpg",
+        "picUrlSq": "https://www.dogbreedslist.info/uploads/dog-pictures/german-shepherd-dog-2-3.jpg",
         "description": "Rex is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
     {
@@ -31,51 +31,51 @@ module.exports = [
     },
     {
         "name": "Princhi",
-        "species": "West Highland White Terrier",
+        "species": "Siberian Husky",
         "birthday": "2008-11-11",
         "favoriteFood": "Fish",
-        "picUrl": "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/West-Highland-White-Terrier-2.jpg",
-        "picUrlSq": "https://upload.wikimedia.org/wikipedia/commons/2/2c/West_Highland_White_Terrier_Krakow.jpg",
+        "picUrl": "https://www.dogbreedslist.info/uploads/dog-pictures/siberian-husky-2.jpg",
+        "picUrlSq": "https://www.dogbreedslist.info/uploads/dog-pictures/siberian-husky-2.jpg",
         "description": "Princhi is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
     {
         "name": "Mr. Fluffles",
-        "species": "Poodle",
+        "species": "Pembroke Welsh Corgi",
         "birthday": "2008-11-11",
         "favoriteFood": "Chicken",
-        "picUrl": "https://i.ytimg.com/vi/-57vwuw_aY8/maxresdefault.jpg",
+        "picUrl": "https://www.dogbreedslist.info/uploads/dog-pictures/pembroke-welsh-corgi-2.jpg",
         "price": 9.99,
-        "picUrlSq": "http://www.caninest.com/images/black-poodle.jpg",
+        "picUrlSq": "https://www.dogbreedslist.info/uploads/dog-pictures/pembroke-welsh-corgi-2.jpg",
         "description": "Mr. Fluffles is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
     {
         "name": "Santa's Little Helper",
-        "species": "Mixed",
+        "species": "Rottweiler",
         "birthday": "2008-11-11",
         "favoriteFood": "Pork",
-        "picUrl": "http://via.placeholder.com/350x150",
+        "picUrl": "https://www.dogbreedslist.info/uploads/dog-pictures/rottweiler-2-2.jpg",
         "price": 9.99,
-        "picUrlSq": "http://via.placeholder.com/250x250",
+        "picUrlSq": "https://www.dogbreedslist.info/uploads/dog-pictures/rottweiler-2.jpg",
         "description": "Santa's Little Helper is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
     {
         "name": "Alf",
-        "species": "Alien",
+        "species": "Bulldog",
         "birthday": "2008-11-11",
         "favoriteFood": "Cats",
-        "picUrl": "http://via.placeholder.com/350x150",
+        "picUrl": "https://www.dogbreedslist.info/uploads/dog-pictures/bulldog-2-2.jpg",
         "price": 59.99,
-        "picUrlSq": "http://via.placeholder.com/250x250",
+        "picUrlSq": "https://www.dogbreedslist.info/uploads/dog-pictures/bulldog-2.jpg",
         "description": "Alf is an alien and he's a good alien who loves to play and hang out with his owners. He also likes to nap and enjoys eating cats"
     },
     {
         "name": "Bubba",
-        "species": "Mastiff",
+        "species": "Golden Retriever",
         "birthday": "2008-11-11",
         "favoriteFood": "Dragon",
-        "picUrl": "http://via.placeholder.com/350x150",
+        "picUrl": "https://www.dogbreedslist.info/uploads/dog-pictures/golden-retriever-2-2.jpg",
         "price": 9.99,
-        "picUrlSq": "http://via.placeholder.com/250x250",
+        "picUrlSq": "https://www.dogbreedslist.info/uploads/dog-pictures/golden-retriever-2.jpg",
         "description": "Bubba is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     }
 ]
